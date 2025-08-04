f026ad48096a72f8295a6928d198e5495687c6ff
Herb v/d Dool <herb@3speedhub.com>
2025-08-04 00:45:20 -0400

    Issue #12: create a new hook hook_mailsystem_info

---
+++ mailsystem.api.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * @file
+ * Hooks provided by the MailSystem module.
+ */
+
+/**
+ * Provide mail system definitions.
+ *
+ * Supported keys:
+ * - label: The human readable label, shown in the UI.
+ * - description: The human readable description, shown in the UI.
+ *
+ * @return array
+ *   Array of mail system definitions with label, description and indexed
+ *   by the class name.
+ */
+function hook_mailsystem_info() {
+  $classes['DefaultMailSystem'] = array(
+    'label' => 'Default',
+    'description' => t('The default Backdrop mail backend using PHP\'s mail function.'),
+  );
+  $classes['TestingMailSystem'] = array(
+    'label' => t('Testing'),
+    'description' => t('A mail sending implementation that captures sent messages to a variable.'),
+  );
+  return $classes;
+}
